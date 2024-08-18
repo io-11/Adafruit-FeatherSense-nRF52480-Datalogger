4e7dd2513bae7fbc9aef2ce85ab7fd933ad7d6b9
io-11 <facco.guido11@gmail.com>
2024-08-18 16:47:12 +0200

    Update Ble_datalogger_ext_sens.ino

--- Cap.6 (C arduino)/Ble_datalogger_ext_sens.ino
+++ Cap.6 (C arduino)/Ble_datalogger_ext_sens.ino
@@ -1,5 +1,5 @@
-/*
- * nrf52840 - Bluetooth Data Logger inspired by MJRoBot (Marcelo Rovai) https://www.hackster.io/mjrobot/sensor-datalogger-50e44d
+/* nrf52840 - Bluetooth Data Logger inspired by MJRoBot (Marcelo Rovai)
+ * https://www.hackster.io/mjrobot/sensor-datalogger-50e44d
  * Generic external data via I2C (in this case temperature sensor LM75A by Texas Instruments)
  * 
  * Guido Facco
@@ -7,16 +7,13 @@
 */
 
 #include <bluefruit.h> // adafruit ble
-#include <Arduino.h>  //serve?
 #include "Wire.h" //Libreria arduino per gestione I2C
-
 
 #define LM75A_ADDRESS 0x48 // Indirizzo I2C del sensore LM75A
 #define TEMPERATURE_REGISTER 0x00 //Indirizzo del registro temperatura dove ci sono i dati
 
-int minPeriod=350;  //definisco il periodo minimo in millisecondi di lettura dei dati. 
-                    //Dipende dal sensore che si è collegato. Nel mio caso LM75A ha un periodo minimo di 300ms.
-                    //Lo metto all'inizio in modo da permettere facili modifiche
+int minPeriod=350;  /*definisco il periodo minimo in millisecondi di lettura dei dati. Dipende dal sensore che si è collegato. 
+            Nel mio caso LM75A ha un periodo minimo di 300ms. Lo metto all'inizio in modo da permettere facili modifiche */
 
 /* Online GUID / UUID Generator:
 https://www.guidgenerator.com/online-guid-generator.aspx
@@ -29,8 +26,6 @@ const char* UUID_serv = "64cf715d-f89e-4ec0-b5c5-d10ad9b53bf2";
 // UUids for temperature data
 const char* UUID_temp   = "64cf715e-f89e-4ec0-b5c5-d10ad9b53bf2";
 
-
-
 // BLE identificazione
 BLEDis bledis;
 // BLE Service instanziazione
@@ -38,8 +33,6 @@ BLEService tempServ = BLEService(UUID_serv);
 
 // BLE Characteristics instanziazione
 BLECharacteristic  tempSens = BLECharacteristic(UUID_temp);
-    //BLEFloatCharacteristic  chAY(UUID_ay,  BLERead|BLENotify);
-
 
 //dichiaro le variabili dei led
 int LEDR=13;
@@ -48,86 +41,74 @@ int connect = 0;  //indica lo stato di connessione 0-non_conn  1-conn
 
 void setup() 
 {
-  Wire.begin(); //attivo I2C
-  Serial.begin(115200);
-  setReadRegister();  //funzione definita e scritta a fine pagina , serve per indicare quale registro voglio leggere
-
-
-
-
-  /////////RIMUOVERE SE NON CONNESSO A PC
- while (!Serial); 
-  /////////////     
-
-
-
-
-
-
-  Serial.println("");
-  Serial.println("--------------------------------------------------------------");
-  Serial.println("BLE - Data Logger(Setup)");
-  
-  bool err=false; //flag di errore
-
-  pinMode(LEDR, OUTPUT); // onboard led red set out
-  pinMode(LEDB, OUTPUT); // onboard led blue (connection status led) set out
-  digitalWrite(LEDR, LOW); // led red off
-  digitalWrite(LEDB, LOW); // led blue off
-
-  // init BLE
+    Wire.begin(); //attivo I2C
+    Serial.begin(115200);
+    setReadRegister();  //funzione definita e scritta a fine pagina , serve per indicare quale registro voglio leggere
     
-  if (!Bluefruit.begin()) 
-  {
-    Serial.println("BLE: failed");
-    err=true;
-  }
-  Bluefruit.setName("Datalogger nRF52840");
-  Bluefruit.setTxPower(4);    // Check bluefruit.h for supported values
-  Bluefruit.Periph.setConnectCallback(connect_callback);
-  Serial.println("BLE: ok");
-
-  // error: flash led forever
-  if (err)
-  {
-    Serial.println("Init error. System halted (non è stato possibile inizializzare ble)");
-    while(1)
+    /////////RIMUOVERE SE NON CONNESSO A PC
+    while (!Serial); 
+    /////////////     
+    
+    Serial.println("");
+    Serial.println("--------------------------------------------------------------");
+    Serial.println("BLE - Data Logger(Setup)");
+    
+    bool err=false;     //flag di errore
+    
+    pinMode(LEDR, OUTPUT);      // onboard led red set out
+    pinMode(LEDB, OUTPUT);      // onboard led blue (connection status led) set out
+    digitalWrite(LEDR, LOW);     // led red off
+    digitalWrite(LEDB, LOW);     // led blue off
+    
+    // init BLE
+    if (!Bluefruit.begin()) 
     {
-      digitalWrite(LEDR, LOW);  //led off
-      delay(500); 
-      digitalWrite(LEDR, HIGH); // led on
-      delay(500);
- 
-    } 
-  }
-
-  // BLE service
-  // correct sequence:
-  // set BLE name > advertised service > add characteristics > add service > set initial values > advertise
-
- // Configure and Start Device Information Service
-  bledis.setManufacturer("Unipd(GF)");
-  bledis.setModel("Bluefruit Feather Sense nRF52840");
-  bledis.begin();
-  
-  // attivare advertised Service
-  
-  tempServ.begin();
-  
-  // attivare characteristics to the Service
-  tempSens.setProperties(CHR_PROPS_NOTIFY);    //non può ricevere dati dall'esterno
-  tempSens.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);  //sola lettura da un dispositivo esterno
-  tempSens.setFixedLen(4);    //dati di 4 byte (long)
-  tempSens.begin();
+        Serial.println("BLE: failed");
+        err=true;
+    }
+    Bluefruit.setName("Datalogger nRF52840");
+    Bluefruit.setTxPower(4);    // Check bluefruit.h for supported values
+    Bluefruit.Periph.setConnectCallback(connect_callback);
+    Serial.println("BLE: ok");
+    
+    // error: flash led forever
+    if (err)
+    {
+        Serial.println("Init error. System halted (non è stato possibile inizializzare ble)");
+        while(1)
+        {
+            digitalWrite(LEDR, LOW);  //led off
+            delay(500); 
+            digitalWrite(LEDR, HIGH); // led on
+            delay(500);
+        } 
+    }
+    
+    // BLE service
+    // correct sequence:
+    // set BLE name > advertised service > add characteristics > add service > set initial values > advertise
+    
+    // Configurazione e Start Device Information Service
+    bledis.setManufacturer("Unipd(GF)");
+    bledis.setModel("Bluefruit Feather Sense nRF52840");
+    bledis.begin();
+    
+    // attivare advertising Service
+    tempServ.begin();
+    
+    // attivare characteristics to the Service
+    tempSens.setProperties(CHR_PROPS_NOTIFY);    //non può ricevere dati dall'esterno
+    tempSens.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);  //sola lettura da un dispositivo esterno
+    tempSens.setFixedLen(4);    //dati di 4 byte (long)
+    tempSens.begin();
      
-  // start advertising
- // Set up and start advertising
-  startAdv();
-  Serial.println("Advertising started");
-  Serial.println("Bluetooth device active, waiting for connections...");
-  
+    // start advertising
+    // Set up and start advertising
+    startAdv();
+    Serial.println("Advertising started");
+    Serial.println("Bluetooth device active, waiting for connections...");
 }
-
+    
 
   
 int check=0;  //variabile ausiliaria usata per mostrare solo una volta(==0) l'avviso di riconnessione sul serial monitor
@@ -137,7 +118,7 @@ float t=0;
 
 void loop() 
 {
-  //queso if permette di visualizzare 1 sola volra il messaggio di riconnessione sul serial monitor quando la connessione è persa
+  //questo if mostra 1 sola volta il messaggio di riconnessione sul serial monitor quando la connessione è persa
   if(!Bluefruit.connected() && connect>=1 && check==0)
   {
     // central disconnected:
@@ -148,7 +129,7 @@ void loop()
 
   long preMillis = millis();  //tempo di riferimento va dichiarato fuori dal while
 
-  //questo invece esegue il necessario per leggere il dato dal registro del sensore e inviarlo tramite BLe
+  //questo invece esegue il necessario per leggere il dato dal registro del sensore e inviarlo tramite BLE
   while (Bluefruit.connected()) 
   {    
     if(connect==0)
@@ -160,32 +141,28 @@ void loop()
 
     //eseguo la lettura ogni minPeriod ma con una funzione non bloccante 
 
-    
-    if(millis()>=preMillis+minPeriod) //se il tempo letto ora è maggiore a quello precedente+il periodo indicato all'inizio, entro nel corpo dell'if
+    if(millis()>=preMillis+minPeriod) //se il tempo letto ora è maggiore a quello precedente+il periodo indicato all'inizio,
+                                  //entro nel corpo dell'if
     {
-    preMillis=millis(); //aggiorno il valore del tempo salvato
-    float t= readTemperature(); //non è una funzione di libreria , ma è definita e scritta sotto
-    Serial.print(i);
-    Serial.print("  Temperature: ");
-    Serial.print(t);
-    Serial.println(" °C");
-    tempSens.notify32(t); 
-    i++;
+        preMillis=millis(); //aggiorno il valore del tempo salvato
+        float t= readTemperature(); //non è una funzione di libreria , ma è definita e scritta sotto
+        Serial.print(i);
+        Serial.print("  Temperature: ");
+        Serial.print(t);
+        Serial.println(" °C");
+        tempSens.notify32(t); 
+        i++;
     }
     
-  } // still here while central connected
-
-  
+  } //resta nel while quando è connesso
 } 
-
-
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Legge la temperatura e trasforma i bit nel valore della temperatura in °C
 float readTemperature() 
 {
-  Wire.requestFrom(LM75A_ADDRESS, 2);//prepara la comunicazione con il sensore mandando l'indirizzo del sensore seguito da un 1(read) e si aspetta 2 byte di trasmissione, dopo i quali manda uno stop
-  //while (Wire.available() < 2);
+  Wire.requestFrom(LM75A_ADDRESS, 2);//prepara la comunicazione con il sensore mandando l'indirizzo del sensore seguito da 
+                                      //un 1(read) e si aspetta 2 byte di trasmissione, dopo i quali manda uno stop
   uint8_t msb = Wire.read();
   uint8_t lsb = Wire.read();
 
@@ -194,18 +171,14 @@ float readTemperature()
   return temperatureC;
 }
 
-
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Setta il registro in cui voglio leggere i dati della temperatura
 void setReadRegister()
 {
-  Wire.beginTransmission(LM75A_ADDRESS); //prepara la comunicazione con il sensore mandando l'indirizzo del sensore seguito da un0 0(write)
+  Wire.beginTransmission(LM75A_ADDRESS); //prepara la comunicazione con il sensore
   Wire.write(TEMPERATURE_REGISTER); //scrive nel sensore l'indirizzo del registro da leggere
   Wire.endTransmission();//chiude questa comunicazione
 }
-
-
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Setta e fa partire l'advertising del BLE
@@ -247,4 +220,3 @@ void connect_callback(uint16_t conn_handle)
   Serial.print("Connected to ");
   Serial.println(central_name);
 }
-
